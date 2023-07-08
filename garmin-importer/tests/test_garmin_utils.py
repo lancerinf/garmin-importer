8cbef480c91b8569f896317d406d0ef228c41113
Federico Lancerin <federico.lancerin@gmail.com>
2023-07-08 12:19:27 +0200

    adds tests for clean_activity function

---
+++ garmin-importer/tests/test_garmin_utils.py
@@ -0,0 +1,29 @@
+from garmin_utils import _clean_activity
+
+import unittest
+import json
+
+
+class TestCleanActivityFunction(unittest.TestCase):
+
+    def test__clean_activity_succeeds_with_outdoor_running_activity(self):
+        with open('../resources/garmin_activity_outdoor_running.json') as json_file:
+            outdoor_running_activity = json.load(json_file)
+            clean_activity = _clean_activity(outdoor_running_activity)
+            self.assertTrue(len(clean_activity))
+
+    def test__clean_activity_succeeds_with_indoor_cycling_activity(self):
+        with open('../resources/garmin_activity_indoor_cycling.json') as json_file:
+            indoor_cycling_activity = json.load(json_file)
+            clean_activity = _clean_activity(indoor_cycling_activity)
+            self.assertTrue(len(clean_activity))
+
+    def test__clean_activity_succeeds_with_indoor_swimming_activity(self):
+        with open('../resources/garmin_activity_indoor_swimming.json') as json_file:
+            indoor_swimming_activity = json.load(json_file)
+            clean_activity = _clean_activity(indoor_swimming_activity)
+            self.assertTrue(clean_activity)
+
+
+if __name__ == '__main__':
+    unittest.main()
